e75a71f3d3a2f7ce2a74cf77b820a5db39658d49
ffdsghhf <xxia25837@gmail.com>
2025-08-14 07:09:38 +0800

    feat: Simplify project and remove unnecessary features

---
+++ app/components/dashboard/Nav.vue
@@ -0,0 +1,25 @@
+<script setup>
+const route = useRoute()
+</script>
+
+<template>
+  <section class="flex justify-between">
+    <Tabs
+      v-if="route.path !== '/dashboard/link'"
+      :default-value="route.path"
+      @update:model-value="navigateTo"
+    >
+      <TabsList>
+        <TabsTrigger
+          value="/dashboard/links"
+        >
+          {{ $t('nav.links') }}
+        </TabsTrigger>
+      </TabsList>
+    </Tabs>
+    <slot name="left" />
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
